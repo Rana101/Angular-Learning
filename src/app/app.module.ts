47b39a5faabfe58a9c8c78a4b053463d7658d84d
Rajashekar G Naidu <raju10naidu@gmail.com>
2021-11-24 10:50:03 +0530

    added layouts assets and vendor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { Layout1Component } from './UI/layout/layout1/layout1.component';
+import { Layout2Component } from './UI/layout/layout2/layout2.component';
+import { LayoutBlankComponent } from './UI/layout/layout-blank/layout-blank.component';
+import { LayoutSidenavComponent } from './UI/layout/layout-sidenav/layout-sidenav.component';
+import { LayoutNavbarComponent } from './UI/layout/layout-navbar/layout-navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    Layout1Component,
+    Layout2Component,
+    LayoutBlankComponent,
+    LayoutSidenavComponent,
+    LayoutNavbarComponent
   ],
   imports: [
     BrowserModule,
