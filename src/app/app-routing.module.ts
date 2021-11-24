ff4125d485c55c1e27f3d167369ef4f70b03ed00
Rajashekar G Naidu <raju10naidu@gmail.com>
2021-11-24 10:36:46 +0530

    learning Routes in @angular/router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  /*path?:string -----> used to match the path
+  -> can be used to describe wild card using '**'
+  -> can be used to describe base route using '' (default route)
+     which can be then redirected */
+  {path:'login', component:LoginComponent},
+
+  /*pathMatch?:string ----> 'prefix' or 'full'
+  -> prefix - //default - 
+  */
+  // {path:'', redirectTo:'login', pathMatch:'prefix'},
+
+  {path:'', redirectTo:'login', pathMatch:'full'}, //redirecting from base route to login route using redirect and pathMatch
+
+  { path: '**', component:NotFoundComponent } //goes to not found component whenever wild card path is triggered
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
